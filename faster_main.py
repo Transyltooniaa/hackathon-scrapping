6bf1b56407e680cf1760b7e66e02fd1c6445f39f
Ajitesh Kumar Singh <Ajitesh.Kumar@iiitb.ac.in>
2023-04-16 17:47:53 +0530

    Update faster_main.py

--- faster_main.py
+++ faster_main.py
@@ -2,25 +2,19 @@ import pyttsx3
 import time
 import pandas as pd
 from tabulate import tabulate
+import questionary
 
 # Initialize the text-to-speech engine
 engine = pyttsx3.init()
 engine.setProperty('rate', 195)
 engine.setProperty('volume', 1.5)
 
-# Function to get user's choice
-def get_choice(prompt):
-    engine.say(prompt)
-    engine.runAndWait()
-    choice = input(prompt)
-    return choice
-
-# Function to display options and get user's choice
+# Function to display options and get user's choice using questionary.select
 def display_options(options):
-    for idx, option in enumerate(options, 1):
-        print(f"{idx}. {option}")
-    choice = get_choice("\nEnter your choice here: ")
-    time.sleep(0.1)
+    choice = questionary.select(
+        "Choose an option:",
+        choices=options
+    ).ask()
     return choice
 
 # Welcome message
@@ -28,6 +22,7 @@ print("\nWelcome to the IMDB Scraper!")
 engine.say("Welcome to the IMDB Scraper!")
 print("Today is a pleasant day to watch movies!\n")
 engine.say("Today is a pleasant day to watch movies! Here are some options for you:")
+engine.runAndWait()
 
 # Display sorting options
 options = [
@@ -43,18 +38,17 @@ options = [
 choice = display_options(options)
 
 # Handle user's choice
-if choice == '1':
+if choice == 'By Genre':
     import new_sim
-elif choice == '2':
+elif choice == 'List of Top movies of all time':
     import Topmovies
-elif choice == '3':
+elif choice == 'List of Top Box Office movies this weekend':
     import Topboxoffice
-elif choice == '4':
+elif choice == 'List of Most popular movies of all time':
     import Popularmovie
-elif choice == '5':
+elif choice == 'Would you like to go into the Indian movie database?':
     import Indianmovie
-
-elif choice == '6':
+elif choice == 'See your watchlist\n':
     with open('watchlist.txt') as f:
         datafile = f.readlines()
         dict = {"Movie Name": datafile}
@@ -65,39 +59,40 @@ elif choice == '6':
         engine.say("Thank you for using the IMDB scraper! Have a nice day! see you soon!")
         engine.runAndWait()
         exit(1)
- 
-    
 
+engine.say("Do you want to see the details for any movie?")
+engine.runAndWait()
 # Prompt for movie details
-b = get_choice("Do you want to see the details for any movie? (Press 1 for yes, 0 for no): ")
+b = questionary.select(
+    "Do you want to see the details for any movie?",
+    choices=["Yes", "No"]
+).ask()
+
 time.sleep(1)
-if b == '1':
+if b == 'Yes':
     import movie_search_main
     print("\nDo you want to add the movie in your watchlist?")
-    print("Press 1 for yes and 0 for no")
     engine.say("Do you want to add the movie in your watchlist?")
-    engine.say("Press 1 for yes and 0 for no")
-    engine.say("Enter your choice here: ")
-    engine.runAndWait()
-    c=int(input("Enter your choice here: "))
-    if c==1:
+    c = questionary.select(
+        "Choose an option:",
+        choices=["Yes", "No"]
+    ).ask()
+    if c == 'Yes':
         def check():
             with open('watchlist.txt') as f:
                 datafile = f.readlines()
             for line in datafile:
                 if movie_search_main.name in line:
-            # found = True # Not necessary
                     return True
-            return False  
-        fptr=open("watchlist.txt","a")
-        if (check()==False):
+            return False
+        fptr = open("watchlist.txt", "a")
+        if not check():
             fptr.write(movie_search_main.name + "\n")
         else:
             print("Movie already exists in watchlist")
             engine.say("Movie already exists in watchlist")
             engine.runAndWait()
         fptr.close()
-
 
 print("\nThank you for using the IMDB scraper! ")
 engine.say("Thank you for using the IMDB scraper! Have a nice day! see you soon!")
